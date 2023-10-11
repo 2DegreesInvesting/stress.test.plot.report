23d263bd908b816ee640fa80af4016f7efe56dcb
bertrand <bertrand.gallice.pro@gmail.com>
2023-10-11 16:24:22 +0200

    WIP

--- R/prepare_for_pd_plot.R
+++ R/prepare_for_pd_plot.R
@@ -1,10 +1,5 @@
 prepare_for_pd_plot <- function(data) {
   out <- data %>%
-  filter(
-    scenario_geography == "Global",
-    investor_name == "Meta Investor",
-    portfolio_name == "Meta Investor"
-  ) %>%
   select(ald_sector, term, shock_year_arg, pd_change_sector_shock) %>%
   distinct()
   out
