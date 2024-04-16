b5cecf714fcadfb9183e19c58cdf38e5462ddfda
bertrand <bertrand.gallice.pro@gmail.com>
2024-04-16 15:55:03 +0200

    add arguments to trajectories df

--- R/data_load_trajectories.R
+++ R/data_load_trajectories.R
@@ -9,9 +9,9 @@
 #'
 main_data_load_trajectories_data_from_file <- function(
     crispy_outputs_dir,
-    granularity, param_cols = c("run_id", "year")) {
+    ...) {
   company_trajectories_data <- load_multiple_trajectories(crispy_outputs_dir) |>
-    main_data_load_trajectories_data(granularity = granularity, param_cols = param_cols)
+    main_data_load_trajectories_data(...)
   return(company_trajectories_data)
 }
 
@@ -24,8 +24,11 @@ main_data_load_trajectories_data_from_file <- function(
 #' @export
 #'
 main_data_load_trajectories_data <- function(company_trajectories_data, granularity, param_cols = c(
-                                               "run_id", "year"
-                                             )) {
+  "run_id", "year","scenario_geography", "baseline_scenario",
+  "shock_scenario", "risk_free_rate", "discount_rate", "div_netprofit_prop_coef",
+  "carbon_price_model", "market_passthrough", "financial_stimulus",
+  "growth_rate", "shock_year"
+)) {
   group_cols <- unique(c(granularity, param_cols))
 
   company_trajectories_data <- company_trajectories_data |>
@@ -60,6 +63,17 @@ load_multiple_trajectories <- function(crispy_outputs_dir) {
         ald_business_unit = "c",
         year = "d",
         phase_out = "d",
+        baseline_scenario_arg = "c",
+        shock_scenario_arg = "c",
+        scenario_geography_arg = "c",
+        risk_free_rate_arg = "d",
+        discount_rate_arg = "d",
+        div_netprofit_prop_coef_arg = "d",
+        growth_rate_arg = "d",
+        shock_year_arg = "d",
+        carbon_price_model_arg = "c",
+        market_passthrough_arg = "d",
+        financial_stimulus_arg = "d",
         production_plan_company_technology = "d",
         production_baseline_scenario = "d",
         production_target_scenario = "d",
@@ -71,6 +85,19 @@ load_multiple_trajectories <- function(crispy_outputs_dir) {
         discounted_net_profits_baseline_scenario = "d",
         discounted_net_profits_shock_scenario = "d"
       )
+    ) |>
+    dplyr::rename(
+      baseline_scenario = baseline_scenario_arg,
+      shock_scenario = shock_scenario_arg,
+      scenario_geography = scenario_geography_arg,
+      risk_free_rate=risk_free_rate_arg,
+      discount_rate=discount_rate_arg,
+      div_netprofit_prop_coef=div_netprofit_prop_coef_arg,
+      growth_rate=growth_rate_arg,
+      shock_year=shock_year_arg,
+      carbon_price_model=carbon_price_model_arg,
+      market_passthrough=market_passthrough_arg,
+      financial_stimulus=financial_stimulus_arg
     ) |>
     dplyr::filter(.data$year < max(.data$year)) # removes last year that is NA
   })
